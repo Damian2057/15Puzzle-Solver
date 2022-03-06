60540d8cc986e3f2ef5e0150edf70fae5622cda4
Damian2057 <ddamian2057@gmail.com>
2022-03-06 14:01:38 +0100

    Create of needed class

---
+++ src/main/java/org/example/fileSystem/FileReader.java
@@ -0,0 +1,8 @@
+package org.example.fileSystem;
+
+public class FileReader implements AutoCloseable{
+    @Override
+    public void close() throws Exception {
+
+    }
+}
---
+++ src/main/java/org/example/fileSystem/FileWriter.java
@@ -0,0 +1,8 @@
+package org.example.fileSystem;
+
+public class FileWriter implements AutoCloseable{
+    @Override
+    public void close() throws Exception {
+
+    }
+}
