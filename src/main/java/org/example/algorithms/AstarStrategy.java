3883a4ac937aaa66ac91c0d6cd6ea2f792d7038a
Damian2057 <ddamian2057@gmail.com>
2022-03-11 16:56:42 +0100

    The beginning of the AStar algorithm implementation

--- src/main/java/org/example/algorithms/AstarStrategy.java
+++ src/main/java/org/example/algorithms/AstarStrategy.java
@@ -1,4 +1,70 @@
 package org.example.algorithms;
 
+import org.example.Exceptions.SolutionException;
+import org.example.model.PuzzleBoard;
+import org.example.model.StatsCollector;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
 public class AstarStrategy {
+
+    private PuzzleBoard utilityBoard;
+
+    private ArrayList<PuzzleBoard> allBoards = new ArrayList<>();
+    private StatsCollector statsCollector;
+    private String algoritmType;
+
+    public AstarStrategy(@NotNull PuzzleBoard utilityBoard, @NotNull String algoritmType
+            , String sol, String stats) {
+        this.utilityBoard = utilityBoard;
+        this.algoritmType = algoritmType;
+        statsCollector = new StatsCollector(sol,stats);
+        try {
+            switch (algoritmType) {
+                case "manh" -> {
+                    statsCollector.startTime();
+                    recursionSolverM();
+                }
+                case "hamm" -> {
+                    statsCollector.startTime();
+                    recursionSolverH();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void recursionSolverM() {
+        try {
+            if(utilityBoard.checkValidation()) {
+                //checking if the board is already solved
+                statsCollector.endWithSollution(utilityBoard);
+                return;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new SolutionException("Error getting solution");
+        }
+    }
+
+    public void recursionSolverH() {
+        try {
+            if(utilityBoard.checkValidation()) {
+                //checking if the board is already solved
+                statsCollector.endWithSollution(utilityBoard);
+                return;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new SolutionException("Error getting solution");
+        }
+    }
+
+    public void doStepBySign() {
+
+
+    }
 }
