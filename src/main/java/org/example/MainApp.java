a189cbf3b40a4cdbbd79f50041adb1b2cccfbaa8
Damian2057 <ddamian2057@gmail.com>
2022-03-13 12:56:41 +0100

    Connecting the program to the appropriate call

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -10,29 +10,28 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-//        try {
-//            org.example.model.CheckArgs.checkArguments(args);
-//        } catch (org.example.Exceptions.ArgsException e) {
-//            System.out.println(e.getMessage());
-//            return;
-//        }
+        try {
+            org.example.model.CheckArgs.checkArguments(args);
+        } catch (org.example.Exceptions.ArgsException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
 
         try {
             FileFactory f = new FileFactory();
-            byte[][] board = f.getPuzzle("C:\\Users\\Damian\\Desktop\\test.txt");
+            byte[][] board = f.getPuzzle(args[2]);
             PuzzleBoard puzzleBoard = new PuzzleBoard(board, f.getWidth() , f.getHeight());
 
-            System.out.println(puzzleBoard.toString());
-            String xd = "bfs";
-            switch (xd) {
+            //System.out.println(puzzleBoard.toString());
+            switch (args[0]) {
                 case "bfs" -> {
-                    BreadthStrategy acrossStrategy = new BreadthStrategy(puzzleBoard, "RUDL","C:\\Users\\Damian\\Desktop\\sol.txt","C:\\Users\\Damian\\Desktop\\sts.txt");
+                    BreadthStrategy acrossStrategy = new BreadthStrategy(puzzleBoard, args[1],args[3],args[4]);
                     System.out.println(acrossStrategy.getUtilityBoard().toString());
                 } case "dfs" -> {
-                    DeeperStrategy deeperStrategy = new DeeperStrategy(puzzleBoard,"LUDR","C:\\Users\\Damian\\Desktop\\sol.txt","C:\\Users\\Damian\\Desktop\\sts.txt");
+                    DeeperStrategy deeperStrategy = new DeeperStrategy(puzzleBoard,args[1],args[3],args[4]);
                     System.out.println(deeperStrategy.getUtilityBoard().toString());
                 } case "astr" -> {
-                    AstarStrategy astarStrategy = new AstarStrategy(puzzleBoard,"hamm","C:\\Users\\Damian\\Desktop\\sol.txt","C:\\Users\\Damian\\Desktop\\sts.txt");
+                    AstarStrategy astarStrategy = new AstarStrategy(puzzleBoard,args[1],args[3],args[4]);
                     System.out.println(astarStrategy.getUtilityBoard().toString());
                 } default -> {
                     System.out.println("Incorrect algorithms.strategy");
